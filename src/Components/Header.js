60d730f48668008fbb8eeb94509260ac044c1192
Meg McCauley <memi.mc14@gmail.com>
2022-05-05 17:11:05 -0600

    added styling to the header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from "react"
 import {Link, useNavigate} from "react-router-dom"
 import "../styles/header.css"
 import axios from "axios"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 export default function Header(props) {
     const [search, setSearch] = useState('')
